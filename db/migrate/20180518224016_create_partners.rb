c4849e06ec854c22091294be77f2742b253fe227
gigashock3u <gigashock3u@hotmail.com>
2018-05-21 13:27:00 -0400

    create db table partners

--- db/migrate/20180518224016_create_partners.rb
+++ db/migrate/20180518224016_create_partners.rb
@@ -1,6 +1,10 @@
 class CreatePartners < ActiveRecord::Migration[5.2]
   def self.up
     create_table :partners do |t|
+      t.string :name
+      t.string :last_name
+      t.integer :rut, unique: true
+      t.timestamps
     end
   end
 
